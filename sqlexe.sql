a00a4846f9d1fb015a0e6e3c0a5077d3dce5993f
kdhital14 <ed.dhital@gmail.com>
2021-03-23 12:13:53 -0700

    Second question from sql exercise completed.

--- sqlexe.sql
+++ sqlexe.sql
@@ -7,3 +7,12 @@ The output should display the most expensive items first.
 
 SELECT productName AS 'NAME' , productLine AS 'Product Line' , buyPrice AS 'Buy Price' FROM products
 ORDER BY BuyPrice DESC; 
+
+/* 
+2.	Write a query to display the first name, last name, and city for all customers from Germany.
+Columns should display as “First Name”, “Last Name”, and “City”.
+The output should be sorted by the customer’s last name (ascending).
+*/
+
+SELECT contactFirstName AS 'First Name', contactLastName AS 'Last Name' ,city AS 'City' FROM customers
+WHERE country = 'Germany' ORDER BY contactLastName ASC;
